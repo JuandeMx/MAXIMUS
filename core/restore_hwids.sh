d5f9c1d918c0a6e41217c0aea3cf21fafb6b7d58
JuandeMx <tu-email@ejemplo.com>
2026-05-05 16:06:31 -0600

    Add bulk HWID restore script generated from WhatsApp chat

---
+++ core/restore_hwids.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+echo 'Importando 45 clientes HWID...'
+useradd -M -s /bin/false ec38b016a343a9afd61bd62fd22ea854 2>/dev/null
+echo "ec38b016a343a9afd61bd62fd22ea854:ec38b016a343a9afd61bd62fd22ea854" | chpasswd
+sed -i '/^ec38b016a343a9afd61bd62fd22ea854:/d' /etc/MaximusVpsMx/users.db
+echo "ec38b016a343a9afd61bd62fd22ea854:HWID_INV:2026-06-04:ec38b016a343a9afd61bd62fd22ea854:1:+54_9_11_6847-9109" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false a6e70a62fdcc625c53830d290ea252a9 2>/dev/null
+echo "a6e70a62fdcc625c53830d290ea252a9:a6e70a62fdcc625c53830d290ea252a9" | chpasswd
+sed -i '/^a6e70a62fdcc625c53830d290ea252a9:/d' /etc/MaximusVpsMx/users.db
+echo "a6e70a62fdcc625c53830d290ea252a9:HWID_INV:2026-06-04:a6e70a62fdcc625c53830d290ea252a9:1:+54_9_3786_61-4771" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 251275bfbad17cc8bbdc311e2a8d71ce 2>/dev/null
+echo "251275bfbad17cc8bbdc311e2a8d71ce:251275bfbad17cc8bbdc311e2a8d71ce" | chpasswd
+sed -i '/^251275bfbad17cc8bbdc311e2a8d71ce:/d' /etc/MaximusVpsMx/users.db
+echo "251275bfbad17cc8bbdc311e2a8d71ce:HWID_INV:2026-06-04:251275bfbad17cc8bbdc311e2a8d71ce:1:+54_9_3856_47-5753" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 8c41b2619a5d5dceb3e07ea8b105f218 2>/dev/null
+echo "8c41b2619a5d5dceb3e07ea8b105f218:8c41b2619a5d5dceb3e07ea8b105f218" | chpasswd
+sed -i '/^8c41b2619a5d5dceb3e07ea8b105f218:/d' /etc/MaximusVpsMx/users.db
+echo "8c41b2619a5d5dceb3e07ea8b105f218:HWID_INV:2026-06-04:8c41b2619a5d5dceb3e07ea8b105f218:1:+54_9_3731_63-8903" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false c521ae3b2dca0f4064590abffbcb2045 2>/dev/null
+echo "c521ae3b2dca0f4064590abffbcb2045:c521ae3b2dca0f4064590abffbcb2045" | chpasswd
+sed -i '/^c521ae3b2dca0f4064590abffbcb2045:/d' /etc/MaximusVpsMx/users.db
+echo "c521ae3b2dca0f4064590abffbcb2045:HWID_INV:2026-06-04:c521ae3b2dca0f4064590abffbcb2045:1:+54_9_11_5379-2772" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false a42c8599fd6912dd675a7dc2ab2983eb 2>/dev/null
+echo "a42c8599fd6912dd675a7dc2ab2983eb:a42c8599fd6912dd675a7dc2ab2983eb" | chpasswd
+sed -i '/^a42c8599fd6912dd675a7dc2ab2983eb:/d' /etc/MaximusVpsMx/users.db
+echo "a42c8599fd6912dd675a7dc2ab2983eb:HWID_INV:2026-06-04:a42c8599fd6912dd675a7dc2ab2983eb:1:+54_9_11_3592-6573" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false a6d6b6cb6f92b728356dca7138e184eb 2>/dev/null
+echo "a6d6b6cb6f92b728356dca7138e184eb:a6d6b6cb6f92b728356dca7138e184eb" | chpasswd
+sed -i '/^a6d6b6cb6f92b728356dca7138e184eb:/d' /etc/MaximusVpsMx/users.db
+echo "a6d6b6cb6f92b728356dca7138e184eb:HWID_INV:2026-06-04:a6d6b6cb6f92b728356dca7138e184eb:1:+54_9_3835_55-0684" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 9b2177cf018ccbadead9918316190070 2>/dev/null
+echo "9b2177cf018ccbadead9918316190070:9b2177cf018ccbadead9918316190070" | chpasswd
+sed -i '/^9b2177cf018ccbadead9918316190070:/d' /etc/MaximusVpsMx/users.db
+echo "9b2177cf018ccbadead9918316190070:HWID_INV:2026-06-04:9b2177cf018ccbadead9918316190070:1:+54_9_380_458-2609" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false e1fa2620d34fe0d4fd9f4c20b03f1b39 2>/dev/null
+echo "e1fa2620d34fe0d4fd9f4c20b03f1b39:e1fa2620d34fe0d4fd9f4c20b03f1b39" | chpasswd
+sed -i '/^e1fa2620d34fe0d4fd9f4c20b03f1b39:/d' /etc/MaximusVpsMx/users.db
+echo "e1fa2620d34fe0d4fd9f4c20b03f1b39:HWID_INV:2026-06-04:e1fa2620d34fe0d4fd9f4c20b03f1b39:1:+54_9_376_465-4808" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 252f3b89fde0008dffb68fbba6f70440 2>/dev/null
+echo "252f3b89fde0008dffb68fbba6f70440:252f3b89fde0008dffb68fbba6f70440" | chpasswd
+sed -i '/^252f3b89fde0008dffb68fbba6f70440:/d' /etc/MaximusVpsMx/users.db
+echo "252f3b89fde0008dffb68fbba6f70440:HWID_INV:2026-06-04:252f3b89fde0008dffb68fbba6f70440:1:+54_9_3844_46-6086" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 2772f1f6ccce68631ee83bf69ce8606d 2>/dev/null
+echo "2772f1f6ccce68631ee83bf69ce8606d:2772f1f6ccce68631ee83bf69ce8606d" | chpasswd
+sed -i '/^2772f1f6ccce68631ee83bf69ce8606d:/d' /etc/MaximusVpsMx/users.db
+echo "2772f1f6ccce68631ee83bf69ce8606d:HWID_INV:2026-06-04:2772f1f6ccce68631ee83bf69ce8606d:1:+54_9_364_484-0831" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 3a8185436d55bd7066d37cf204dee8a0 2>/dev/null
+echo "3a8185436d55bd7066d37cf204dee8a0:3a8185436d55bd7066d37cf204dee8a0" | chpasswd
+sed -i '/^3a8185436d55bd7066d37cf204dee8a0:/d' /etc/MaximusVpsMx/users.db
+echo "3a8185436d55bd7066d37cf204dee8a0:HWID_INV:2026-06-04:3a8185436d55bd7066d37cf204dee8a0:1:+54_9_380_487-6406" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false ca5193bb818bf7260b275417d690461a 2>/dev/null
+echo "ca5193bb818bf7260b275417d690461a:ca5193bb818bf7260b275417d690461a" | chpasswd
+sed -i '/^ca5193bb818bf7260b275417d690461a:/d' /etc/MaximusVpsMx/users.db
+echo "ca5193bb818bf7260b275417d690461a:HWID_INV:2026-06-04:ca5193bb818bf7260b275417d690461a:1:+54_9_3777_84-4915" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 3dc510eda3e7cae17892d28c03baf3a8 2>/dev/null
+echo "3dc510eda3e7cae17892d28c03baf3a8:3dc510eda3e7cae17892d28c03baf3a8" | chpasswd
+sed -i '/^3dc510eda3e7cae17892d28c03baf3a8:/d' /etc/MaximusVpsMx/users.db
+echo "3dc510eda3e7cae17892d28c03baf3a8:HWID_INV:2026-06-04:3dc510eda3e7cae17892d28c03baf3a8:1:+54_9_3887_41-1078" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 9fda7b347edaaeef873ebb2175499c7f 2>/dev/null
+echo "9fda7b347edaaeef873ebb2175499c7f:9fda7b347edaaeef873ebb2175499c7f" | chpasswd
+sed -i '/^9fda7b347edaaeef873ebb2175499c7f:/d' /etc/MaximusVpsMx/users.db
+echo "9fda7b347edaaeef873ebb2175499c7f:HWID_INV:2026-06-04:9fda7b347edaaeef873ebb2175499c7f:1:+54_9_3873_69-9707" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 7fb712c5f1d6c03042c951a37472bb22 2>/dev/null
+echo "7fb712c5f1d6c03042c951a37472bb22:7fb712c5f1d6c03042c951a37472bb22" | chpasswd
+sed -i '/^7fb712c5f1d6c03042c951a37472bb22:/d' /etc/MaximusVpsMx/users.db
+echo "7fb712c5f1d6c03042c951a37472bb22:HWID_INV:2026-06-04:7fb712c5f1d6c03042c951a37472bb22:1:+54_9_11_3435-7015" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false f9c997979450fcb9155313bc767c0d71 2>/dev/null
+echo "f9c997979450fcb9155313bc767c0d71:f9c997979450fcb9155313bc767c0d71" | chpasswd
+sed -i '/^f9c997979450fcb9155313bc767c0d71:/d' /etc/MaximusVpsMx/users.db
+echo "f9c997979450fcb9155313bc767c0d71:HWID_INV:2026-06-04:f9c997979450fcb9155313bc767c0d71:1:+54_9_11_6625-1421" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false df92b70c6d8d4fc06501058279d4714a 2>/dev/null
+echo "df92b70c6d8d4fc06501058279d4714a:df92b70c6d8d4fc06501058279d4714a" | chpasswd
+sed -i '/^df92b70c6d8d4fc06501058279d4714a:/d' /etc/MaximusVpsMx/users.db
+echo "df92b70c6d8d4fc06501058279d4714a:HWID_INV:2026-06-04:df92b70c6d8d4fc06501058279d4714a:1:+54_9_3832_41-7399" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false cf5be87939badf540c84abc2e2ff4d3a 2>/dev/null
+echo "cf5be87939badf540c84abc2e2ff4d3a:cf5be87939badf540c84abc2e2ff4d3a" | chpasswd
+sed -i '/^cf5be87939badf540c84abc2e2ff4d3a:/d' /etc/MaximusVpsMx/users.db
+echo "cf5be87939badf540c84abc2e2ff4d3a:HWID_INV:2026-06-04:cf5be87939badf540c84abc2e2ff4d3a:1:+54_9_3878_62-5899" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 58886bfa37925d20d7e6e81d817ed69a 2>/dev/null
+echo "58886bfa37925d20d7e6e81d817ed69a:58886bfa37925d20d7e6e81d817ed69a" | chpasswd
+sed -i '/^58886bfa37925d20d7e6e81d817ed69a:/d' /etc/MaximusVpsMx/users.db
+echo "58886bfa37925d20d7e6e81d817ed69a:HWID_INV:2026-06-04:58886bfa37925d20d7e6e81d817ed69a:1:+54_9_3854_44-5448" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 80ae3cbdb49731afbdf609a2aad33039 2>/dev/null
+echo "80ae3cbdb49731afbdf609a2aad33039:80ae3cbdb49731afbdf609a2aad33039" | chpasswd
+sed -i '/^80ae3cbdb49731afbdf609a2aad33039:/d' /etc/MaximusVpsMx/users.db
+echo "80ae3cbdb49731afbdf609a2aad33039:HWID_INV:2026-06-04:80ae3cbdb49731afbdf609a2aad33039:1:+54_9_3772_64-1543" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 03a7585e1792f47c6fff5f667c3fd3e0 2>/dev/null
+echo "03a7585e1792f47c6fff5f667c3fd3e0:03a7585e1792f47c6fff5f667c3fd3e0" | chpasswd
+sed -i '/^03a7585e1792f47c6fff5f667c3fd3e0:/d' /etc/MaximusVpsMx/users.db
+echo "03a7585e1792f47c6fff5f667c3fd3e0:HWID_INV:2026-06-04:03a7585e1792f47c6fff5f667c3fd3e0:1:+54_9_3751_66-4877" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 1fcad91fe6606835400250db855f49e6 2>/dev/null
+echo "1fcad91fe6606835400250db855f49e6:1fcad91fe6606835400250db855f49e6" | chpasswd
+sed -i '/^1fcad91fe6606835400250db855f49e6:/d' /etc/MaximusVpsMx/users.db
+echo "1fcad91fe6606835400250db855f49e6:HWID_INV:2026-06-04:1fcad91fe6606835400250db855f49e6:1:+54_9_364_414-6278" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false bcda011594b203eb33f883284a5377da 2>/dev/null
+echo "bcda011594b203eb33f883284a5377da:bcda011594b203eb33f883284a5377da" | chpasswd
+sed -i '/^bcda011594b203eb33f883284a5377da:/d' /etc/MaximusVpsMx/users.db
+echo "bcda011594b203eb33f883284a5377da:HWID_INV:2026-06-04:bcda011594b203eb33f883284a5377da:1:+54_9_381_644-3836" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false bf472d74ad9371b478e305755d7c3d79 2>/dev/null
+echo "bf472d74ad9371b478e305755d7c3d79:bf472d74ad9371b478e305755d7c3d79" | chpasswd
+sed -i '/^bf472d74ad9371b478e305755d7c3d79:/d' /etc/MaximusVpsMx/users.db
+echo "bf472d74ad9371b478e305755d7c3d79:HWID_INV:2026-06-04:bf472d74ad9371b478e305755d7c3d79:1:+54_9_11_2559-0255" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 4c5e4b68796172b45e86b0d529b57a48 2>/dev/null
+echo "4c5e4b68796172b45e86b0d529b57a48:4c5e4b68796172b45e86b0d529b57a48" | chpasswd
+sed -i '/^4c5e4b68796172b45e86b0d529b57a48:/d' /etc/MaximusVpsMx/users.db
+echo "4c5e4b68796172b45e86b0d529b57a48:HWID_INV:2026-06-04:4c5e4b68796172b45e86b0d529b57a48:1:+54_9_388_415-2341" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false d9320d86e8d0d3db3bc0b406114ac729 2>/dev/null
+echo "d9320d86e8d0d3db3bc0b406114ac729:d9320d86e8d0d3db3bc0b406114ac729" | chpasswd
+sed -i '/^d9320d86e8d0d3db3bc0b406114ac729:/d' /etc/MaximusVpsMx/users.db
+echo "d9320d86e8d0d3db3bc0b406114ac729:HWID_INV:2026-06-04:d9320d86e8d0d3db3bc0b406114ac729:1:+54_9_364_468-2346" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 6de57de72bfaa495ce18879cb572afd5 2>/dev/null
+echo "6de57de72bfaa495ce18879cb572afd5:6de57de72bfaa495ce18879cb572afd5" | chpasswd
+sed -i '/^6de57de72bfaa495ce18879cb572afd5:/d' /etc/MaximusVpsMx/users.db
+echo "6de57de72bfaa495ce18879cb572afd5:HWID_INV:2026-06-04:6de57de72bfaa495ce18879cb572afd5:1:+54_9_3782_41-6620" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 2b746fa32c8b27488ae80b2acf6b304e 2>/dev/null
+echo "2b746fa32c8b27488ae80b2acf6b304e:2b746fa32c8b27488ae80b2acf6b304e" | chpasswd
+sed -i '/^2b746fa32c8b27488ae80b2acf6b304e:/d' /etc/MaximusVpsMx/users.db
+echo "2b746fa32c8b27488ae80b2acf6b304e:HWID_INV:2026-06-04:2b746fa32c8b27488ae80b2acf6b304e:1:+54_9_388_682-9405" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false b1e39d36424caf17b6e63991ce6c945a 2>/dev/null
+echo "b1e39d36424caf17b6e63991ce6c945a:b1e39d36424caf17b6e63991ce6c945a" | chpasswd
+sed -i '/^b1e39d36424caf17b6e63991ce6c945a:/d' /etc/MaximusVpsMx/users.db
+echo "b1e39d36424caf17b6e63991ce6c945a:HWID_INV:2026-06-04:b1e39d36424caf17b6e63991ce6c945a:1:+54_9_376_462-8611" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false ce7e9c80ade1f8cd33d7fd9fa64a40b3 2>/dev/null
+echo "ce7e9c80ade1f8cd33d7fd9fa64a40b3:ce7e9c80ade1f8cd33d7fd9fa64a40b3" | chpasswd
+sed -i '/^ce7e9c80ade1f8cd33d7fd9fa64a40b3:/d' /etc/MaximusVpsMx/users.db
+echo "ce7e9c80ade1f8cd33d7fd9fa64a40b3:HWID_INV:2026-06-04:ce7e9c80ade1f8cd33d7fd9fa64a40b3:1:+54_9_381_496-8582" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 0eb87a4f36f148309d1b889e17848360 2>/dev/null
+echo "0eb87a4f36f148309d1b889e17848360:0eb87a4f36f148309d1b889e17848360" | chpasswd
+sed -i '/^0eb87a4f36f148309d1b889e17848360:/d' /etc/MaximusVpsMx/users.db
+echo "0eb87a4f36f148309d1b889e17848360:HWID_INV:2026-06-04:0eb87a4f36f148309d1b889e17848360:1:+54_9_3877_40-2931" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 6c9f00c6dc3563515e6e4786742fc440 2>/dev/null
+echo "6c9f00c6dc3563515e6e4786742fc440:6c9f00c6dc3563515e6e4786742fc440" | chpasswd
+sed -i '/^6c9f00c6dc3563515e6e4786742fc440:/d' /etc/MaximusVpsMx/users.db
+echo "6c9f00c6dc3563515e6e4786742fc440:HWID_INV:2026-06-04:6c9f00c6dc3563515e6e4786742fc440:1:+54_9_3751_66-5572" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 1c6afe9095e30cf3c5496ea7a1fb324b 2>/dev/null
+echo "1c6afe9095e30cf3c5496ea7a1fb324b:1c6afe9095e30cf3c5496ea7a1fb324b" | chpasswd
+sed -i '/^1c6afe9095e30cf3c5496ea7a1fb324b:/d' /etc/MaximusVpsMx/users.db
+echo "1c6afe9095e30cf3c5496ea7a1fb324b:HWID_INV:2026-06-04:1c6afe9095e30cf3c5496ea7a1fb324b:1:+54_9_11_6619-1426" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 099e158bcad1b6b45f783da2df964765 2>/dev/null
+echo "099e158bcad1b6b45f783da2df964765:099e158bcad1b6b45f783da2df964765" | chpasswd
+sed -i '/^099e158bcad1b6b45f783da2df964765:/d' /etc/MaximusVpsMx/users.db
+echo "099e158bcad1b6b45f783da2df964765:HWID_INV:2026-06-04:099e158bcad1b6b45f783da2df964765:1:Maríadelosangelesarguello" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 6c5efa02d767ecf9182e65ad70569786 2>/dev/null
+echo "6c5efa02d767ecf9182e65ad70569786:6c5efa02d767ecf9182e65ad70569786" | chpasswd
+sed -i '/^6c5efa02d767ecf9182e65ad70569786:/d' /etc/MaximusVpsMx/users.db
+echo "6c5efa02d767ecf9182e65ad70569786:HWID_INV:2026-06-04:6c5efa02d767ecf9182e65ad70569786:1:+54_9_3489_70-3168" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 4188a3dbe88367781f2159631c360414 2>/dev/null
+echo "4188a3dbe88367781f2159631c360414:4188a3dbe88367781f2159631c360414" | chpasswd
+sed -i '/^4188a3dbe88367781f2159631c360414:/d' /etc/MaximusVpsMx/users.db
+echo "4188a3dbe88367781f2159631c360414:HWID_INV:2026-06-04:4188a3dbe88367781f2159631c360414:1:+54_9_3476_59-6880" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false d340ae5d71abae3f50ac92bf63d5094e 2>/dev/null
+echo "d340ae5d71abae3f50ac92bf63d5094e:d340ae5d71abae3f50ac92bf63d5094e" | chpasswd
+sed -i '/^d340ae5d71abae3f50ac92bf63d5094e:/d' /etc/MaximusVpsMx/users.db
+echo "d340ae5d71abae3f50ac92bf63d5094e:HWID_INV:2026-06-04:d340ae5d71abae3f50ac92bf63d5094e:1:+54_9_3878_64-9627" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 180132a3d4fbf80e23824c1162014283 2>/dev/null
+echo "180132a3d4fbf80e23824c1162014283:180132a3d4fbf80e23824c1162014283" | chpasswd
+sed -i '/^180132a3d4fbf80e23824c1162014283:/d' /etc/MaximusVpsMx/users.db
+echo "180132a3d4fbf80e23824c1162014283:HWID_INV:2026-06-04:180132a3d4fbf80e23824c1162014283:1:+54_9_3835_69-2719" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false f21a8b2d7769475e9f4ceb5af0fb1935 2>/dev/null
+echo "f21a8b2d7769475e9f4ceb5af0fb1935:f21a8b2d7769475e9f4ceb5af0fb1935" | chpasswd
+sed -i '/^f21a8b2d7769475e9f4ceb5af0fb1935:/d' /etc/MaximusVpsMx/users.db
+echo "f21a8b2d7769475e9f4ceb5af0fb1935:HWID_INV:2026-06-04:f21a8b2d7769475e9f4ceb5af0fb1935:1:+54_9_3844_54-1915" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false a93c72a7eccad01a6c2965504f6b7fcf 2>/dev/null
+echo "a93c72a7eccad01a6c2965504f6b7fcf:a93c72a7eccad01a6c2965504f6b7fcf" | chpasswd
+sed -i '/^a93c72a7eccad01a6c2965504f6b7fcf:/d' /etc/MaximusVpsMx/users.db
+echo "a93c72a7eccad01a6c2965504f6b7fcf:HWID_INV:2026-06-04:a93c72a7eccad01a6c2965504f6b7fcf:1:+54_9_3751_40-0043" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 8ff2830ee9e98164894ba22989d71bda 2>/dev/null
+echo "8ff2830ee9e98164894ba22989d71bda:8ff2830ee9e98164894ba22989d71bda" | chpasswd
+sed -i '/^8ff2830ee9e98164894ba22989d71bda:/d' /etc/MaximusVpsMx/users.db
+echo "8ff2830ee9e98164894ba22989d71bda:HWID_INV:2026-06-04:8ff2830ee9e98164894ba22989d71bda:1:+54_9_3857_46-6057" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 742eadfdb8170cd733c273a7442ff413 2>/dev/null
+echo "742eadfdb8170cd733c273a7442ff413:742eadfdb8170cd733c273a7442ff413" | chpasswd
+sed -i '/^742eadfdb8170cd733c273a7442ff413:/d' /etc/MaximusVpsMx/users.db
+echo "742eadfdb8170cd733c273a7442ff413:HWID_INV:2026-06-04:742eadfdb8170cd733c273a7442ff413:1:+54_9_364_415-5854" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false a610d0485b7a6c83a8b80bc080c43aa9 2>/dev/null
+echo "a610d0485b7a6c83a8b80bc080c43aa9:a610d0485b7a6c83a8b80bc080c43aa9" | chpasswd
+sed -i '/^a610d0485b7a6c83a8b80bc080c43aa9:/d' /etc/MaximusVpsMx/users.db
+echo "a610d0485b7a6c83a8b80bc080c43aa9:HWID_INV:2026-06-04:a610d0485b7a6c83a8b80bc080c43aa9:1:+54_9_376_483-5800" >> /etc/MaximusVpsMx/users.db
+useradd -M -s /bin/false 180132a3d4fbf80e23824c1162014283 2>/dev/null
+echo "180132a3d4fbf80e23824c1162014283:180132a3d4fbf80e23824c1162014283" | chpasswd
+sed -i '/^180132a3d4fbf80e23824c1162014283:/d' /etc/MaximusVpsMx/users.db
+echo "180132a3d4fbf80e23824c1162014283:HWID_INV:2026-06-04:180132a3d4fbf80e23824c1162014283:1:+54_9_11_3089-6834" >> /etc/MaximusVpsMx/users.db
+echo 'Importacion completada!'
